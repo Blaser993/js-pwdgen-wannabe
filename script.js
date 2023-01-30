375af6b3736a646addd8023087296330b2341e36
Blaser993 <corradodepinto.design@gmail.com>
2023-01-30 14:36:44 +0100

    NEW FEAT la psw viene scritta sul dovumento

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 
+// 1. Chiedi all’utente il suo nome
 
+let nome = prompt( "scrivi il tuo nome");
+console.log(nome);
 
-
-
-// 1. Chiedi all’utente il suo nome,
 // 2. poi chiedi il suo cognome,
+
+let cognome = prompt( "scrivi il tuo cognome");
+console.log(cognome);
 // 3. poi chiedi il suo colore preferito
+
+let colore = prompt( "scrivi il tuo colore preferito");
+console.log(colore);
 // 4. e un numero
+
+let numero = prompt( "scrivi un numero");
+console.log(numero);
 // 5. Infine scrivi sulla pagina il risultato della concatenazione di: <nome><cognome><colorePreferito><numero></numero>
+
+
+document.write(nome + cognome + colore + numero)
+console.log(nome + cognome + colore + numero);
