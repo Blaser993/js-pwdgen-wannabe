0243a9f5bcf7ef4f0aa29ed69d22f5c9c7bbaab9
Blaser993 <corradodepinto.design@gmail.com>
2023-01-31 09:46:22 +0100

    REFACT post correzione

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+console.log("hello!")
+
+let nome, cognome, colore, numero
+
+
 
 // 1. Chiedi all’utente il suo nome
 
-let nome = prompt( "scrivi il tuo nome");
+nome = prompt( "scrivi il tuo nome");
 console.log(nome);
 
 // 2. poi chiedi il suo cognome,
 
-let cognome = prompt( "scrivi il tuo cognome");
+cognome = prompt( "scrivi il tuo cognome");
 console.log(cognome);
 // 3. poi chiedi il suo colore preferito
 
-let colore = prompt( "scrivi il tuo colore preferito");
+colore = prompt( "scrivi il tuo colore preferito");
 console.log(colore);
 // 4. e un numero
 
-let numero = prompt( "scrivi un numero");
+numero = prompt( "scrivi un numero");
 console.log(numero);
-// 5. Infine scrivi sulla pagina il risultato della concatenazione di: <nome><cognome><colorePreferito><numero></numero>
+// 5. genera la psw concatenando le variabili
+
+const psw = nome + cognome + colore + numero
+console.log(psw);
 
 
-document.write(nome + cognome + colore + numero)
-console.log(nome + cognome + colore + numero);
+// 6. Infine scrivi sulla pagina il risultato 
+const h1Element = document.getElementById("psw");
+h1Element.innerHTML = psw
